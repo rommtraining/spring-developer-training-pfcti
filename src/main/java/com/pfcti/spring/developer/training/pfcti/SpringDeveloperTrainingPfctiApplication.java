eb9dc671afaa03b84aa89812c9ac459a056a7ecd
Roger Morera <roger.morera@grupopromerica.com>
2023-04-18 18:06:47 -0600

    Primer commit Spring

---
+++ src/main/java/com/pfcti/spring/developer/training/pfcti/SpringDeveloperTrainingPfctiApplication.java
@@ -0,0 +1,13 @@
+package com.pfcti.spring.developer.training.pfcti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeveloperTrainingPfctiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeveloperTrainingPfctiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfcti/spring/developer/training/pfcti/SpringDeveloperTrainingPfctiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfcti.spring.developer.training.pfcti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeveloperTrainingPfctiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
